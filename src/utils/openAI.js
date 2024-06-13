425433061a6135b24ccc8965f8c3d4ba0702106d
Pratysh Anand <anandpratyush1666@gmail.com>
2024-06-13 22:41:09 +0530

    update api

--- src/utils/openAI.js
+++ src/utils/openAI.js
@@ -2,7 +2,7 @@ import OpenAI from 'openai';
 import { openAI_key } from './Constants';
 
 const openAI = new OpenAI({
-  apiKey: openAI_key,
+  apiKey: "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",
   dangerouslyAllowBrowser: true  // This is the default and can be omitted
 });
 
